3853effd0b94f370fcaa8512b6a7412c327f9787
xubing19160231 <1962892111@qq.com>
2016-10-08 07:18:56 -0700

    Add work-19160231

---
+++ commit/20160929/19160231/1/p17_2.1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int q;
+long int qw;
+signed long int qwe;
+unsigned int as;
+unsigned long int asd;
+short int z;
+signed short int zx;
+unsigned short int x;
+
+printf("int size=%d\n",sizeof(q));
+printf("long int size=%d\n",sizeof(qw));
+printf("signed long int size=%d\n",sizeof(qwe));
+printf("unsigned long int size=%d]n",sizeof(asd));
+printf("unsihned int size=%d\n",sizeof(as));
+printf("short int size=%d\n",sizeof(z));
+printf("signed short int size=%d\n",sizeof(zx));
+printf("unsigned short int size=%d\n",sizeof(x));
+
+return 0;
+}
