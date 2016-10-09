22694c7054aca53908f58519b078206b31dba9b5
ShaoRanRan <923550589@qq.com>
2016-10-09 22:34:52 +0800

    Add work for 20160929 - 22161921

---
+++ commit/20160929/22161921/1/P17_2.1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int  c;
+    unsigned int  i;
+    short int z;
+    unsigned short int x;
+
+    printf("int  size=%lu\n",sizeof(c));
+    printf("unsigned size=%lu\n",sizeof(i));
+    printf("short int size=%lu\n",sizeof(z));
+    printf("unsigned short int size=%lu\n",sizeof(x));
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+  
