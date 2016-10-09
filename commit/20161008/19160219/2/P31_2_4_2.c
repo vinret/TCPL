e0a7f3b7c7ff7d106f76b1a09cef1869220fc3bc
shenjiahao 19160219 <sy5622_5@126.com>
2016-10-09 21:39:00 +0800

    Add work-19160219

---
+++ commit/20161008/19160219/2/P31_2_4_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int x,a,b,c;
+printf("please input x,a,b and c\n");
+scanf("%d,%d,%d,%d",&x,&a,&b,&c);
+sqrt(fabs(x))!=(4*a)/(b*c);
+return 0;
+}
+
