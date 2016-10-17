e92ac6da0f174f6c8401d9961beafe0f13853daf
Jiang <932988567@qq.com>
2016-10-17 19:48:02 +0800

    Add work for 20161008 ,22161926 YiYun Jiang

---
+++ commit/20161008/22161926/6/tcpl_2_8/tcpl_2_8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,n;
+    int a,b;
+    scanf("%d%d",&x,&n);
+    a=~(~0<<n)&x;
+    b=a<<(32-n);
+    x=(x>>n)|b;
+    printf("x=%d\n",x);
+    return 0;
+}
