2be01d72dd5414aefb81d618c9041cc5a5cb7c81
qingcailan <APTX4689qcl@163.com>
2016-10-18 20:48:19 +0800

    Add for work 20161008_22161907_qingcailan

---
+++ commit/20161008/22161907/1/P31-2-4-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int a,b,c;
+   float x,d;
+   scanf("%d,%d,%d,%f", &a,&b,&c,&x);
+   y=abs(x);
+   printf("%d\n",sqrt(y)!=4*a/b*c);
+   return 0;
+}
