1ad13026b7ecccec576e960416b14ecc0874bc61
22161917 <1274024886@qq.com>
2016-10-21 06:41:27 -0700

    Add work for 20161020-22161917-Chen,yixin

---
+++ commit/20161020/22161917/5/5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    int     i;
+    char   ch;
+    scanf("%f%d %c %f",&a,&i,&ch,&b);
+    printf("%c %d %.2f %.2f\n",ch,i,a,b);
+
+    return 0;
+}
+
