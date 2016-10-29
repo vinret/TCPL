7178d2580b4584b59200b82573b1a1fa2f5db646
shaohaojie <893938092@qq.com>
2016-10-29 14:59:00 +0800

    Add work for 20161020 - 22161922

---
+++ commit/20161020/22161922/8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+int showbit(int n)
+{
+        if(n>1){
+        showbit(n/2);
+}
+        printf("%d",n%2);
+}
+int main()
+{
+        int a;
+        printf("输入的整数:\n");
+        scanf("%d",&a);       
+        printf("输出的二进制:");
+        showbit(a);
+        printf("\n");
+        printf("输出的八进制:%o\n",a);
+        printf("输出的十六进制:%x\n",a);
+
+return 0;
+}
