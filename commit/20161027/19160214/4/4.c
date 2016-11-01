eee161d4843d420389dd0fb2ca8e4a28c1edee5f
lihao19160214 <878279317@qq.com>
2016-11-01 08:35:21 -0700

    Add work-19160214

---
+++ commit/20161027/19160214/4/4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{int x,n,i,k,j,c;
+ char b;
+ scanf("%d %c",&x,&b);
+for(n=1;;n++)
+{if(2*n*n>x)break;
+  
+}
+n=n-1;
+for(i=n;i>=2;i--)
+ {for(j=0;j<=n-i;j++)
+  printf(" ");
+  for(k=1;k<=2*i-1;k++)
+    printf("%c",b);
+    printf("\n");
+}
+
+
+ for(i=1;i<=n;i++)
+{for(j=0;j<=n-i;j++)
+  printf(" ");
+  for(k=1;k<=2*i-1;k++)
+    printf("%c",b);
+    printf("\n");
+}
+c=x-2*n*n+1;
+printf("%d\n",c);
+return 0;
+}
