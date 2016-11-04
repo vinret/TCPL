edc8fb7a5dbc9ebbace5b196eaa06aeb881aa953
ChenLuting <1277633781@qq.com>
2016-11-04 16:21:22 +0800

    Add work for 20161103-22161920,Chen Luting

---
+++ commit/20161103/22161920/2/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{    
+long long a[10],b[10],c[10],x=0;    
+int i=0,j=0;
+scanf("%lld",&x);    
+for(i;i<x;i++)    {        
+scanf("%lld %lld %lld",&a[i],&b[i],&c[i]);    
+}    
+for(j;j<x;j++)    {        
+if((a[j]+b[j])>c[j])    {            
+printf("Case #%d: true\n",j+1);}        
+else                    
+printf("Case #%d: false\n",j+1);            
+}
+return 0;
+}
---
+++ commit/20161103/22161920/4/4.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int i,j,l,max,k=0;
+  char min;
+  char a[1001];
+  char d[1001];
+  char b[1001]={0};
+  gets(a);
+  l=strlen(a);
+  for(i=0;i<l;i++)
+  {
+    if(a[i]>='A'&&a[i]<='Z')
+    {
+      
+      a[i]=a[i]+32;
+    }
+    
+    b[a[i]]++;
+  }
+  max=b[a[0]];
+  for(i=0;i<l;i++)
+  {
+    if(max<=b[a[i]]&&a[i]>='a'&&a[i]<='z')
+    {
+      max=b[a[i]];
+        d[k++]=a[i];  
+    }
+    
+  }
+  min=d[0];
+  for(i=0;i<k;i++)
+  {
+    if(min>d[i])
+    min=d[i];
+  }
+  
+  printf("%c %d\n", min,max);
+  return 0;
+ } 
