9725cd64665d7c6555f84721918921570a37e2c5
19160207 <194463810@qq.com>
2016-11-20 22:44:29 +0800

    Add work for 20161103-19160207

---
+++ commit/20161103/19160207/7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int is_prime(int n)
+   {
+   if(n<=1) return -1;
+   int i;
+   for(i=2;i<=sqrt(n);i++)
+     if(n%i==0) return -1;
+   return 0;
+   }
+
+  int main()
+  {
+     int n,i=0;
+     for(n=2;i<=1000;n++)
+     {
+       if(is_prime(n)==0)
+       {
+         i++;
+         if(i>=100&&i<=1000)
+           printf("%d:%d",i,n);
+       }
+     }
+     printf("\n");
+     return 0;
+  }
+  return 0;
+}
+
+ 
+
+       
