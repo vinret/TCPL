d0f5a7151317f71ff82d965ff2480fdb0ad7739f
lvyizuo <15005522566@163.com>
2016-12-05 10:03:27 +0800

    Add work for 20161201 22161906lvyizuo

---
+++ commit/20161201/22161906/6/6.c
@@ -0,0 +1,39 @@
+//教材 P145 例 6.13。 
+#include<stdio.h>
+#define ROW 3
+#define COL 4
+int main()
+{
+    int matrixA[ROW][COL],matrixB[COL][ROW];
+    int i,j;
+    printf("请输入矩阵A中的元素：");
+    printf("%d*%d:\n",ROW,COL);
+    for(i=0;i<ROW;i++)
+    {
+       for(j=0;j<COL;j++)
+       {
+           scanf("%d",&matrixA[i][j]);
+       }
+    }
+
+    for(i=0;i<ROW;i++)
+    {
+        for(j=0;j<COL;j++)
+       {
+         matrixB[j][i]= matrixA[i][j];
+       }
+    }
+    printf("转置矩阵B:");
+    printf("%d*%d:\n",COL,ROW);
+    for(i=0;i<COL;i++)
+    {
+       for(j=0;j<ROW;j++)
+       {
+           printf("%8d",matrixB[i][j]);
+       }
+       printf("\n");
+    }
+    return 0;
+}
+
+
