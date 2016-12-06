27dc7d687ecd4877e8674883fcd6ee3da3df4294
tangchen19160212 <1654169282@qq.com>
2016-12-06 02:51:03 -0800

    Add work-19160212

---
+++ commit/20161124/19160212/8/8.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int is_prime(int n);
+
+int main()
+{
+int n,i,k=1,M,N,x,s[10001],l;
+for(l=0;l<10001;l++)
+{
+s[l]=0;
+}
+l=1;
+scanf("%d%d",&M,&N);
+for(n=2;k<M;n++)
+{
+i=is_prime(n);
+if(i) k++;
+}
+for(n=n;k<=N;n++)
+{
+i=is_prime(n);
+if(i) {s[l]=n;l++;k++;}
+}
+for(l=1;l<10001;l++)
+{
+if(l%10==0) {printf("%d\n",s[l]);l++;}
+if(l==N-M+1) {printf("%d\n",s[l]);break;}
+printf("%d ",s[l]);
+}
+return 0;
+}
+
+int is_prime(int n)
+{
+int i;
+for(i=2;i<=n-1;i++)
+if(n%i==0) break;
+if(i<n) return 0;
+else return i;
+}
+
