7728cdd63663b59754889e36e772397770c6e62b
19160207 <194463810@qq.com>
2016-12-08 22:57:44 +0800

    add homework for 20161201-19160207

--- commit/20161201/19160207/7/1.c
+++ commit/20161201/19160207/7/1.c
@@ -1,44 +1,55 @@
 #include<stdio.h>
-#define ROW 3
-#define COL 4
 int main()
 {
-		int a[ROW][COL],b[ROW][COL],c[ROW][COL];
-		int i,j;
+		int r,s,t;
+		int m,j,i;
+		printf("请输入矩阵A的行列和矩阵B的列数: ");
+		scanf("%d %d %d",&r,&s,&t);
+		int a[r][s];
+		int b[s][t];
+		int c[r][t];
 
-		printf("请输入矩阵A,3*4:\n");
-		for(i=0;i<ROW;i++)
-				for(j=0;j<COL;j++)
+		printf("请输入矩阵A:\n");
+		for(i=0;i<r;i++)
+				for(j=0;j<s;j++)
 						scanf("%d",&a[i][j]);
-		printf("矩阵Ａ为:\n");
-		for(i=0;i<ROW;i++)
+		printf("请输入矩阵B:\n");
+		for(i=0;i<s;i++)
+				for(j=0;j<t;j++)
+						scanf("%d",&b[i][j]);
+
+/*		printf("矩阵Ａ为:\n");
+		for(i=0;i<r;i++)
 		{
-				for(j=0;j<COL;j++)
+				for(j=0;j<s;j++)
 						printf("%d ",a[i][j]);
 				printf("\n");
 		}
-
-		printf("请输入矩阵B,3*4:\n");
-		for(i=0;i<ROW;i++)
-				for(j=0;j<COL;j++)
-						scanf("%d",&b[i][j]);
-		printf("矩阵Ｂ为:\n");
-		for(i=0;i<ROW;i++)
+		printf("矩阵Ｂ为：\n");
+		for(i=0;i<s;i++)
 		{
-				for(j=0;j<COL;j++)
+				for(j=0;j<t;j++)
 						printf("%d ",b[i][j]);
 				printf("\n");
-		}
-//矩阵相乘
-        for(i=0;i<ROW;i++)
-				for(j=0;j<COL;j++)
-						c[i][j]=a[i][j]*b[i][j];
-		printf("A,B矩阵相乘为：\n");
-		for(i=0;i<ROW;i++)
+		}*/
+
+//矩阵相乘						
+		for(i=0;i<r;i++)
 		{
-				for(j=0;j<COL;j++)
+				for(j=0;j<t;j++)
+				{
+						c[i][j]=0;
+						for(m=0;m<s;m++)
+								c[i][j]+=a[i][m]*b[m][j];
+				}
+		}
+
+		printf("矩阵Ａ,Ｂ相乘得矩阵Ｃ:\n");
+		for(i=0;i<r;i++)
+		{
+				for(j=0;j<t;j++)
 						printf("%d ",c[i][j]);
-				printf("\n");
+		        printf("\n");
 		}
 		return 0;
 }
