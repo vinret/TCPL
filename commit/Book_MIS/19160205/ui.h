18667b349870c3061de6fad24d476775fc59e2a8
shijiankang <1291549369@qq.com>
2016-12-30 21:04:03 +0800

    Add work for Book_MIS-19160205

---
+++ commit/Book_MIS/19160205/ui.h
@@ -0,0 +1,5 @@
+int menu();
+int processCommand(Book aryBook[],Book*paryPrice[],int*pSize,char cmd[]);
+int showItem(Book aryBook[],int*pSize,int id);
+int showAll(Book aryBook[],int*pSize);
+int showAllOrderByPrice(Book*paryPrice[],int*pSize);
