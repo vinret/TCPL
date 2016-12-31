1b44a2060b9bc7b4abdc6e7bcda405b5a6ead60b
19160239 <1304824784@qq.com>
2016-12-31 22:50:22 +0800

    Add work for 19160239  Book_MIS

---
+++ commit/Book_MIS/19160239/Book.h
@@ -0,0 +1,23 @@
+#ifndef"Book.h"
+#define BOOKNUM 1000;
+struct book
+{
+  char name[1000];
+  char author[1000];
+  int isbn;
+  float price;
+};
+  int size;
+  typedef struct book Book;
+
+Book bookAry[BOOKNUM];
+  int menu;
+  int errCmd();
+  Book* insert(Book *pBookAry,int *pSize);
+  Book* search(Book *pBookAry,int size,char * bname);
+  int delete(Book *pBookAry,int *pSize,char * bname);
+  Book* update(Book *pBookAry,int size,char * bname);
+  int save(Book *pBookAry,int size);
+  char *my_gets(char str[],size_t len);
+  int loadArray(Book *pBookAry);
+#endif
